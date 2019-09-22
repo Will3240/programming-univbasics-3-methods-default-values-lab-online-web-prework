59f7130b180877f4eb0eac59a6cc595aab1e6c72
William <teststuffreally@gmail.com>
2019-09-22 13:36:21 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,5 @@
-def add meal_choice
-  "A plate of #{protein}with#
+def "A plate of #{protein}with#
    add{veg1}and#{veg2}."
-  "what a nutritious meal!" 
-  
-end
+  add meal_choice
+  "what a nutritious meal!"
+  end
